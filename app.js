09e3239c31e203058b923c5f2f418fb75c1c4369
Pavel <pishchalaups@belhard.com>
2020-02-21 08:36:17 +0300

    003 Data types

---
+++ app.js
@@ -0,0 +1,29 @@
+// 003
+
+// comments
+
+//      single line
+
+/*
+        multi-line
+*/
+
+/**
+ *      JSDoc comments
+ */
+
+ // Data types
+/** 1. Primitives
+ *      1-1. Number - 123, 123.123, NaN, Infinity 
+ *      1-2. String - "Hello", 'Hello', for variable -> `Hello`
+ *      1-3. Boolean - true, false
+ *      1-4. Null - null
+ *      1-5. Undefined - undefined
+ *      1-6. Symbol() - for uniq identifier // example - console.log(Symbol());
+ * 
+ *  2. Objects - reference type (ссылочный)
+ *      2-1. Object - { name: 'Pit', age; 25 } // example - console.table({});
+ *      2-2. Array - [1, 2, 3]
+ *      2-3. Function - function foo() {}
+ *      2-4. Date - new Date();
+ *  */  
