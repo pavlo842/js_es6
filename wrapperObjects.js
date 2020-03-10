09cc5e3e5acd4885a052a09dc72c2302f0872486
Pavel <pishchalaups@belhard.com>
2020-03-10 12:13:28 +0300

    023 wrapper objects

---
+++ wrapperObjects.js
@@ -0,0 +1,24 @@
+// wrapper objects - Объекты обертки
+
+let str = 'Hello world';
+
+// console.log(str.length);
+// console.log(str.slice(1, -1));
+
+// console.dir(String);
+
+const strObj = new String('Hello');
+
+console.log(strObj); // String {"Hello"}
+// 0: "H"
+// 1: "e"
+// 2: "l"
+// 3: "l"
+// 4: "o"
+// length: 5
+
+
+// console.dir(Number);
+const numObj = new Number(123456789);
+console.log(numObj); // Number {123456789}
+
