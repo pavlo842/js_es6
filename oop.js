3098f5aa2e632189bbf28a655cb072109a30aa80
pavel <pishchalaups@belhard.com>
2020-05-20 16:26:18 +0300

    047 Введение в ООП. Функции конструкторы - end

--- oop.js
+++ oop.js
@@ -10,8 +10,11 @@ const str = new String('Hello world');
 // console.log(str);
 
 // функция конструктор
-
+// по соглашение пишуться с большой буквы
 function Product(brand, price, discount) {
+    // 1. Создает новый объект
+    // 2. на этот объект создается ссылка
+    // 3. возвращает это объект
     console.log(this);
     this.brand = brand;
     this.price = price;
@@ -22,6 +25,7 @@ function Product(brand, price, discount) {
 }
 
 const apple = new Product('Apple', 100, 15);
+const samsung = new Product('Samsung', 90, 14);
 console.log(apple);
+console.log(samsung);
 
-13.50
